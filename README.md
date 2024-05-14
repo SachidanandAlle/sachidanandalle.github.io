4c06f0b3bfca0bcd0b487876214680224dfe0661
Sachidanand Alle <sachidanand.alle@gmail.com>
2024-05-14 23:47:38 +0000

    Refactor + Support NV Hosted NIM Endpoint
    Signed-off-by: Sachidanand Alle <sachidanand.alle@gmail.com>

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ and open http://localhost:9097 in browser.
 You can change default port `9097` in `client.py`
 
 > This is to proxy all the request via local server. Hence this tool is meant for development/debugging only.
+
+
+If you are using 
--- client.py
+++ client.py
@@ -1,10 +1,13 @@
+import argparse
 import os.path
 import pathlib
 import socketserver
 from http.server import SimpleHTTPRequestHandler
-from urllib.request import urlopen, Request
+from urllib.request import urlopen
 
-PORT = 9097
+import requests
+
+session = requests.Session()
 
 
 class MyProxy(SimpleHTTPRequestHandler):
@@ -12,40 +15,64 @@ class MyProxy(SimpleHTTPRequestHandler):
         url = self.path[1:]
         # print(f"GET: {self.path} => {url}")
 
-        self.send_response(200)
-        self.end_headers()
-
         if not url:
             url = "index.html"
-
         if url in ["index.html", "vista3dNIM.js", "vista3dNIM.js.map", "favicon.ico"]:
+            self.send_response(200)
+            self.end_headers()
+
             p = pathlib.Path(os.path.abspath(f'./{url}'))
             if url in ["vista3dNIM.js", "vista3dNIM.js.map"]:
                 with open(url, "rb") as f:
                     c = f.read().replace(b"const NIM_PROXY_URL = '';", b"const NIM_PROXY_URL = '/';")
                     self.wfile.write(c)
                 return
+            self.copyfile(urlopen(p.as_uri()), self.wfile)
+            return
 
-            url = p.as_uri()
+        headers = self._proxy_headers()
+        response = session.get(url, headers=headers)
 
-        self.copyfile(urlopen(url), self.wfile)
+        self.send_response(response.status_code)
+        for k, v in response.headers.items():
+            self.send_header(k, v)
+        self.end_headers()
+        self.wfile.write(response.content)
 
     def do_POST(self):
         url = self.path[1:]
         # print(f"POST: {self.path} => {url}")
 
-        self.send_response(200)
-        self.end_headers()
-
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        # print(f"Post Data: {post_data}")
-        # print(f"Post Headers: {self.headers}")
 
-        req = Request(url, method='POST', data=post_data, headers={k: v for k, v in self.headers.items()})
-        self.copyfile(urlopen(req), self.wfile)
+        headers = self._proxy_headers()
+        response = session.post(url, headers=headers, data=post_data, verify=False)
+
+        self.send_response(response.status_code)
+        for k, v in response.headers.items():
+            self.send_header(k, v)
+        self.end_headers()
+        self.wfile.write(response.content)
+
+    def _proxy_headers(self):
+        filters = {"host", "origin", "referer", "cookie", "user-agent"}
+        return {
+            k: v for k, v in self.headers.items() if k.lower() not in filters and not k.lower().startswith("sec-")
+        }
 
 
-httpd = socketserver.ThreadingTCPServer(('', PORT), MyProxy)
-print("Now serving at: http://localhost:" + str(PORT))
-httpd.serve_forever()
+def run_server():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--host", default="", type=str, help="Local/Server Address (default: localhost)")
+    parser.add_argument("-p", "--port", default=9097, type=int, help="Local/Server Port (default: 9097)")
+
+    args = parser.parse_args()
+
+    httpd = socketserver.ThreadingTCPServer((args.host, args.port), MyProxy)
+    print(f"Now serving at: http://{args.host if args.host else 'localhost' }:{args.port}")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run_server()
